313f93cca3245d5e45a58e93d4f9e082034d35c1
abutilmis <abutitamiru5@gmail.com>
2026-08-14 15:38:26 +0300

    change domain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="description"
           content="Play the Bible Quiz and compete for top scores. Answer scripture questions, track your rank, and join the quiz challenge."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://bible-quiz-games.vercel.app/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
